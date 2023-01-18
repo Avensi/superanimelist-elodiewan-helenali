b2270c3022c79a5d9e6f61788750a964afa638c4
ElodieWan <elodiewan2001@gmail.com>
2023-01-18 11:54:08 +0100

    Feat: header, footer, base router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Error404Component} from "./error404/error404.component";
+import {AccueilComponent} from "./accueil/accueil.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AccueilComponent},
+  {path: '**', component: Error404Component}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'top'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { Error404Component } from './error404/error404.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    Error404Component,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
